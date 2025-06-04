88d8b76af7e1a5b99bad68b02b0df0ac782708c3
INKI977 <121192703+INKI977@users.noreply.github.com>
2025-06-04 23:54:09 +0200

    Update route_actions.js

--- routes/route_actions.js
+++ routes/route_actions.js
@@ -1,7 +1,8 @@
 //Action routes
-const{postEcoAction,getEcoAction,getEcoTopContributors, todaysAchievements}=require('../controllers/controller_actions');
+const{postEcoAction,getEcoAction,getEcoTopContributors, todaysAchievements, getAchievment}=require('../controllers/controller_actions');
 const router=require('express').Router();
 router.post('/create-action',postEcoAction)
 router.get('/actions',getEcoAction);
 router.get('/topContributors',getEcoTopContributors);
 router.get('/todaysAchievements',todaysAchievements);
+router.get('/achievement',getAchievement);
