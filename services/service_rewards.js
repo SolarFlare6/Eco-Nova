d64c6885af21f4bb017044a3d1b2d4235e74896e
Pacha21-droid <78153385+Pacha21-droid@users.noreply.github.com>
2025-06-05 10:34:15 +0200

    Updated service_rewards.js

--- services/service_rewards.js
+++ services/service_rewards.js
@@ -1,11 +1,11 @@
-const rewardsModel=require('../models/model_rewards');
-const userModel=require('../models/model_user');
+const Rewards=require('../models/model_rewards');
+const User=require('../models/model_user');
 class RewardsService{
     static async claimReward(userID,rewardID){
     try {
     //Find the user and the reward
-    const reward= await rewardsModel.findById(rewardID);
-    const user=await userModel.findById(userID);
+    const reward= await Rewards.findById(rewardID);
+    const user=await User.findById(userID);
     //If not found or not enough points
     if(!user||!reward)throw new Error('User or Reward not found');
     if(user.points<reward.cost)throw new Error('Not enough points for this reward');
@@ -17,12 +17,25 @@ class RewardsService{
     }catch(error){
      throw error;}
     }
+//Add and save reward
    static async addReward(rewardData){
     try{
-      const reward=new rewardsModel(rewardData);
+      const reward=new Rewards(rewardData);
       await reward.save();  
     }catch(error) { 
     throw error;}
    }
+//Fetch rewards
+static async getRewards(urls = []){
+    try{
+    let backendRewards;
+    if (urls.length > 0) {
+     backendRewards = await Rewards.find({ url: { $in: urls } }); 
+    }else{
+     backendRewards = await Rewards.find();}
+    return [...backendRewards];
+  }catch(error) {
+    console.log('Error fetching events info');
+    throw error;}} 
 }
 module.exports=RewardsService;
