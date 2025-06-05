63f6dceca396840e2e40844248622cd540f33a10
INKI977 <121192703+INKI977@users.noreply.github.com>
2025-06-05 10:47:16 +0200

    Update route_rewards.js

--- routes/route_rewards.js
+++ routes/route_rewards.js
@@ -1,6 +1,7 @@
 //Reward route
-const{claimReward,addReward}=require('../controllers/controller_rewards');
+const{claimReward,addReward,getRewards}=require('../controllers/controller_rewards');
 const router=require('express').Router();
 router.post('/claimReward',claimReward);
 router.post('/addReward',addReward);
+router.get('/rewards',getRewards);
 module.exports=router;
