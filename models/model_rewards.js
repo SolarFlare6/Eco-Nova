6d024381034e1227bd1b2c7588763ab10fba1a19
INKI977 <121192703+INKI977@users.noreply.github.com>
2025-06-05 10:45:11 +0200

    Update model_rewards.js

--- models/model_rewards.js
+++ models/model_rewards.js
@@ -3,6 +3,7 @@ const mongoose=require('mongoose');
 const{Schema}=mongoose;
 const rewardsSchema=new Schema({
 rewardName:String,
+url:String,
 cost:Number});
 const rewardsModel=mongoose.model('rewardsModel',rewardsSchema);
 module.exports=rewardsModel;
